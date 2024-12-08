70c776d127b15a1c5f718f2e02ea389feaecd76e
mrdeadlift <mrdeadlift96@gmail.com>
2024-12-08 10:43:45 +0900

    first commit

---
+++ god-is-saying-lol/__main__.py
@@ -0,0 +1,67 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QDesktopWidget
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import Qt, QUrl
+import leagueofevents
+
+class VideoPlayer(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+        self.initVideo()
+
+    def initUI(self):
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.WindowStaysOnTopHint)
+        self.setWindowTitle("Video Player")
+        screen_resolution = QDesktopWidget().screenGeometry()
+        self.setGeometry(screen_resolution)
+        self.setWindowOpacity(0.7)
+
+        self.video_widget = QVideoWidget(self)
+        self.video_widget.setGeometry(screen_resolution)
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.video_widget)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+        self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.media_player.setVideoOutput(self.video_widget)
+        self.media_player.setVolume(50)
+        self.media_player.stateChanged.connect(self.on_media_state_changed)
+
+    def initVideo(self):
+        # 動画のパスを取得
+        current_dir = os.getcwd()
+        file_path = os.path.join(current_dir, 'video', 'Elshaddai_GodIsSaying_720p.mp4')
+
+        if not os.path.exists(file_path):
+            print("File does not exist:", file_path)
+            return
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(file_path)))
+        self.media_player.play()
+
+    def on_media_state_changed(self, state):
+        if state == QMediaPlayer.StoppedState: 
+            self.media_player.stop()
+            self.close()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.media_player.stop()
+            self.close()
+
+def onDeath():
+    app = QApplication(sys.argv)
+    player = VideoPlayer()
+    player.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    onDeath()
+    leagueofevents.subscribe_to_event("onDeath", onDeath)
+
